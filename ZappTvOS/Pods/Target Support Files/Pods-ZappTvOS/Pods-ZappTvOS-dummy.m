d593553b63a93611d8052030d0e86f01bbb02d1d
kononenkoAnton <a.kononenko@applicaster.com>
2020-06-15 14:57:23 -0400

    Add tvos caching

---
+++ ZappTvOS/Pods/Target Support Files/Pods-ZappTvOS/Pods-ZappTvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZappTvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ZappTvOS
+@end
---
+++ ZappTvOS/Pods/Target Support Files/ZappApple/ZappApple-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZappApple : NSObject
+@end
+@implementation PodsDummy_ZappApple
+@end
