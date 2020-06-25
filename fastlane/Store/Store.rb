4d047324f2b4c16262323cd067ecde4bf96f0d1d
Alex Zchut <a.zchut@applicaster.com>
2020-06-25 10:09:04 +0300

    temp commit (2)

--- fastlane/Store/Store.rb
+++ fastlane/Store/Store.rb
@@ -166,8 +166,8 @@ class Store < BuildType
     )
 
     # add support for push notifications
-    puts(@projectHelper.plugins_for_type("push"))
-    
+    puts("push plugins count: #{@projectHelper.plugins_for_type("push").count}")
+
     if @projectHelper.plugins_for_type("push").count > 0
       @projectHelper.change_system_capability(
         capability: 'com.apple.Push',
