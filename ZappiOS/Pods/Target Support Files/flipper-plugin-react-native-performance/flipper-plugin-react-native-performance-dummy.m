98ccfab72ff885a09d80fb34a42d062d5e21d41a
Alex Zchut <alexzchut@users.noreply.github.com>
2020-07-09 12:44:19 +0300

    Pre release 21.0.0 (#85)
    * Update pods
    
    * Add change log
    
    Co-authored-by: kononenkoAnton <a.kononenko@applicaster.com>

--- ZappiOS/Pods/Target Support Files/flipper-plugin-react-native-performance/flipper-plugin-react-native-performance-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flipper_plugin_react_native_performance : NSObject
-@end
-@implementation PodsDummy_flipper_plugin_react_native_performance
-@end
