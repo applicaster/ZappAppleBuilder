3313a606943463c328f9e7f5541e08666ff46f24
Pawel Buderaski <pbuderaski@gmail.com>
2020-08-06 15:29:27 +0100

    feat: added linear gradient and svg to autolinking

--- Scripts/autolink-react-dependencies.rb
+++ Scripts/autolink-react-dependencies.rb
@@ -35,4 +35,6 @@ def use_react_native! (options={})
   pod 'DoubleConversion', :podspec => "#{prefix}/third-party-podspecs/DoubleConversion.podspec"
   pod 'glog', :podspec => "#{prefix}/third-party-podspecs/glog.podspec"
   pod 'Folly', :podspec => "#{prefix}/third-party-podspecs/Folly.podspec"
+  pod 'RNSVG', :path => './node_modules/react-native-svg/RNSVG.podspec'
+  pod 'BVLinearGradient', :path => './node_modules/react-native-linear-gradient/BVLinearGradient.podspec'
 end
