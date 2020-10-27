d36247b8b1ed7eb588bea58349cfd13452d489d4
Alex Zchut <a.zchut@applicaster.com>
2020-10-27 23:54:00 +0200

    disable assistance

--- SettingsBundle/BuildSettingsBundle.sh
+++ SettingsBundle/BuildSettingsBundle.sh
@@ -20,13 +20,12 @@ echo "Creating $CODESIGNING_FOLDER_PATH/Settings.bundle for '$CONFIGURATION' bui
 /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:1:Key string 'app_version'" "$SETTINGSBUNDLEPATH"
 /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:1:DefaultValue string '$APPSHORTVERSION ($APPVERSION)'" "$SETTINGSBUNDLEPATH"
 
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:2:Type string 'PSGroupSpecifier'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:2:Title string 'Assistance'" "$SETTINGSBUNDLEPATH"
 
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:2:Type string 'PSGroupSpecifier'" "$SETTINGSBUNDLEPATH"
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:2:Title string 'Assistance'" "$SETTINGSBUNDLEPATH"
-
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:Type string 'PSToggleSwitchSpecifier'" "$SETTINGSBUNDLEPATH"
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:Title string 'Log events'" "$SETTINGSBUNDLEPATH"
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:Key string 'logger_assistance'" "$SETTINGSBUNDLEPATH"
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:DefaultValue bool 'false'" "$SETTINGSBUNDLEPATH"
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:TrueValue bool 'true'" "$SETTINGSBUNDLEPATH"
-/usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:FalseValue bool 'false'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:Type string 'PSToggleSwitchSpecifier'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:Title string 'Log events'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:Key string 'logger_assistance'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:DefaultValue bool 'false'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:TrueValue bool 'true'" "$SETTINGSBUNDLEPATH"
+# /usr/libexec/PlistBuddy -c "Add :PreferenceSpecifiers:3:FalseValue bool 'false'" "$SETTINGSBUNDLEPATH"
