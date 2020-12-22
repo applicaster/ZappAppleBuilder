948b57a1131d6e5e6ffe10904c657f9bb82de699
Alex Zchut <alexzchut@users.noreply.github.com>
2020-12-22 08:43:18 +0200

    feat: Adding support for analytics plugins in tvOS (#181)
    * remove custom settings from the notification service extension
    
    * Adding support for analytics plugins in tvOS
    Updating ZappPlugins to 11.9.0 (support for tvOS)

--- ZappiOS/NotificationServiceExtension/NotificationService.swift
+++ ZappiOS/NotificationServiceExtension/NotificationService.swift
@@ -8,19 +8,6 @@
 
 import UserNotifications
 
-#if AIRSHIP_EXTENSIONS_ENABLED
-
-import AirshipAppExtensions
-class NotificationService: UAMediaAttachmentExtension {
-
-}
-
-#else
-
-#if FIREBASE_EXTENSIONS_ENABLED
-import Firebase
-#endif
-
 //default implementation
 class NotificationService: UNNotificationServiceExtension {
 
@@ -34,20 +21,10 @@ class NotificationService: UNNotificationServiceExtension {
         guard let content = bestAttemptContent else {
             return
         }
-
-        #if FIREBASE_EXTENSIONS_ENABLED
-        Messaging.serviceExtension().populateNotificationContent(content,withContentHandler: contentHandler)
-        #else
         
-        self.getAttachments(for: content.userInfo) { (attachments) in
-            content.attachments = attachments
-            
-            DispatchQueue.main.async {
-                contentHandler(content)
-            }
+        DispatchQueue.main.async {
+            contentHandler(content)
         }
-        
-        #endif
     }
     
     override func serviceExtensionTimeWillExpire() {
@@ -58,96 +35,3 @@ class NotificationService: UNNotificationServiceExtension {
         }
     }
 }
-
-extension NotificationService {
-    /// Helper function to extract Attachments from an userInfo object
-    ///
-    /// - Attribute userInfo: The user info extracted from the notification,
-    ///                       which should contain one of the available keys to create
-    ///                       a rich push notification.
-    public func getAttachments(for userInfo: [AnyHashable: Any], completion: @escaping ([UNNotificationAttachment]) -> Void) {
-        let uniqueString = ProcessInfo.processInfo.globallyUniqueString
-        var attachments:[UNNotificationAttachment] = []
-        
-        if let attachmentURLString = userInfo["image-url-png"] as? String {
-            guard let attachmentURL = URL(string: attachmentURLString),
-                let imageData = try? Data(contentsOf: attachmentURL),
-                let attachment = self.save("\(uniqueString).png", data: imageData, options: nil) else {
-                    completion(attachments)
-                    return
-            }
-            attachments.append(attachment)
-        }
-        else if let attachmentURLString = userInfo["image-url-jpg"] as? String {
-            guard let attachmentURL = URL(string: attachmentURLString),
-                let imageData = try? Data(contentsOf: attachmentURL),
-                let attachment = self.save("\(uniqueString).jpg", data: imageData, options: nil) else {
-                    completion(attachments)
-                    return            }
-            attachments.append(attachment)
-        }
-        else if let attachmentURLString = userInfo["image-url-gif"] as? String {
-            guard let attachmentURL = URL(string: attachmentURLString),
-                let imageData = try? Data(contentsOf: attachmentURL),
-                let attachment = self.save("\(uniqueString).gif", data: imageData, options: nil) else {
-                    completion(attachments)
-                    return
-            }
-            attachments.append(attachment)
-        }
-        else if let attachmentURLString = userInfo["attachment-url"] as? String {
-            guard let attachmentURL = URL(string: attachmentURLString),
-                let imageData = try? Data(contentsOf: attachmentURL),
-                let attachment = self.save(attachmentURL.lastPathComponent, data: imageData, options: nil) else {
-                    completion(attachments)
-                    return
-            }
-            attachments.append(attachment)
-        }
-        else if let attachment = userInfo["com.urbanairship.media_attachment"] as? [AnyHashable: Any],
-            let attachmentURLarray = attachment["url"] as? [String],
-            let attachmentURLString = attachmentURLarray.first {
-            guard let attachmentURL = URL(string: attachmentURLString),
-                let imageData = try? Data(contentsOf: attachmentURL),
-                let attachment = self.save(attachmentURL.lastPathComponent, data: imageData, options: nil) else {
-                    completion(attachments)
-                    return
-            }
-            attachments.append(attachment)
-        }
-
-        completion(attachments)
-    }
-
-    /// Save data object onto disk and return an optional attachment linked to this path.
-    ///
-    /// - Attributes:
-    ///   - identifier: The unique identifier of the attachment.
-    ///                 Use this string to identify the attachment later. If you specify an
-    ///                 empty string, this method creates a unique identifier string for you.
-    ///   - data: The data stored onto disk.
-    ///   - options: A dictionary of options related to the attached file.
-    ///              Use the options to specify meta information about the attachment,
-    ///              such as the clipping rectangle to use for the resulting thumbnail.
-    private func save(_ identifier: String, data: Data, options: [AnyHashable: Any]?) -> UNNotificationAttachment? {
-        // Create paths
-        let directory = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString, isDirectory: true)
-        let fileURL = directory.appendingPathComponent(identifier)
-        // Write data on disk
-        try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
-        try? data.write(to: fileURL, options: [])
-        // Create Notification attachment
-        return try? UNNotificationAttachment(identifier: identifier, url: fileURL, options: options)
-    }
-
-    /// Something went wrong, so maybe we want to clean up and present a fallback to the user.
-    ///
-    /// - Attribute reason: The reason why something went wrong.
-    private func exitGracefully(_ reason: String = "") {
-        guard let copy = bestAttemptContent?.mutableCopy() as? UNMutableNotificationContent else { return }
-        copy.title = reason
-        contentHandler?(copy)
-    }
-}
-
-#endif
