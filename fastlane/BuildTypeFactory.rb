32a255f82c332b332f77ec98f94941ae64181306
Alex Zchut <a.zchut@applicaster.com>
2020-12-28 13:46:36 +0200

    update

--- fastlane/BuildTypeFactory.rb
+++ fastlane/BuildTypeFactory.rb
@@ -12,9 +12,11 @@ class BuildTypeFactory
   end
 
   def perform_signing_validation(options)
+    type_to_validate = options[:type]
+
     # perform validation for all needed env
     build_types_for_use.each do |type_for_use|
-      if type_for_use.build_type == type
+      if type_for_use.build_type == type_to_validate
         # download signing files
         type_for_use.download_signing_files
 
@@ -24,41 +26,41 @@ class BuildTypeFactory
         puts("Perform signing validation for #{type_for_use.class.name}".colorize(:yellow))
         type_for_use.perform_signing_validation
       else 
-        puts("Unable to perform signing validation for #{type} build".colorize(:red))
+        puts("Unable to perform signing validation for #{type_to_prepare} build".colorize(:red))
       end
     end
   end
 
   def prepare_environment(options)
-    type = options[:type]
+    type_to_prepare = options[:type]
 
     # prepare only specific env to build
     build_types_for_use.each do |type_for_use|
-      if type_for_use.build_type == type
+      if type_for_use.build_type == type_to_prepare
         # replace store to enterprise-client if needed
         type_for_use = replace_store_to_enterprise_client_if_needed(type_for_use)
 
         puts("Prepare environment for #{type_for_use.class.name}")
         type_for_use.prepare_environment
       else 
-        puts("Unable to prepare environment for #{type} build".colorize(:red))
+        puts("Unable to prepare environment for #{type_to_prepare} build".colorize(:red))
       end
     end
   end
 
   def build(options)
-    type = options[:type]
+    type_to_build = options[:type]
 
     # build each one of the env
     build_types_for_use.each do |type_for_use|
-      if type_for_use.build_type == type
+      if type_for_use.build_type == type_to_build
         # replace store to enterprise-client if needed
         type_for_use = replace_store_to_enterprise_client_if_needed(type_for_use)
 
         puts("Building for #{type_for_use.class.name}")
         type.build
       else 
-        puts("Unable to build for #{type}".colorize(:red))
+        puts("Unable to build for #{type_to_build}".colorize(:red))
       end
     end
   end
