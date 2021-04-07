f9cc906dbb7fae5be3f10e87c8b434458a7ca953
kononenkoAnton <a.kononenko@applicaster.com>
2021-04-07 11:12:05 -0400

    Ignore upload to appcenter for debug builds (#241)

--- fastlane/Base/BuildType.rb
+++ fastlane/Base/BuildType.rb
@@ -363,8 +363,6 @@ class BuildType < BaseHelper
         ipa: "#{circle_artifacts_folder_path}/#{build_type}/#{@project_helper.scheme}-#{build_type}.ipa",
         dsym: "#{circle_artifacts_folder_path}/#{build_type}/#{@project_helper.scheme}-#{build_type}.app.dSYM.zip"
       )
-      puts('Upload application to MS App Center')
-      @app_center_helper.upload_app(options)
     end
   end
 
